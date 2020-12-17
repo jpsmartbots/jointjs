84f72b182c56632ea10915eb5a8c791f3058e61f
vijay <vijay.shootorder@gmail.com>
2020-12-17 16:46:26 +0530

    first commit!

--- gitpush.js
+++ gitpush.js
@@ -1,10 +1,7 @@
 
 
-
-
 require('simple-git')()
-     .init()
      .add('./*')
      .commit("first commit!")
      .addRemote('origin', 'https://github.com/jpsmartbots/jointjs')
-     .push('origin', 'master');
+     .push(['-u', 'origin', 'master'], () => console.log('done'));
