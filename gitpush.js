71043edc443f822f58e14b7c17a8b68e4cd6c257
vijay <vijay.shootorder@gmail.com>
2020-12-17 17:09:14 +0530

    Intial commit by simplegit

--- gitpush.js
+++ gitpush.js
@@ -1,8 +1,38 @@
-
-
-let x = require('simple-git')()
-     .add('./*')
-     .commit("first commit!")
-     .addRemote('origin', 'https://jpsmartbots@github.com/jpsmartbots/jointjs')
-     .push(['-u', 'origin', 'master'], () => console.log('done'));
-console.log('X='+ JSON.stringify(x));
+// change current directory to repo directory in local
+// shellJs.cd('path/to/repo/folder');
+// Repo name
+const repo = 'jointjs';  //Repo name
+// User name and password of your GitHub
+const userName = 'jpsmartbots';
+const password = 'Smartbots1!';
+// Set up GitHub url like this so no manual entry of user pass needed
+const gitHubUrl = `https://${userName}:${password}@github.com/${userName}/${repo}`;
+// add local git config like username and email
+// simpleGit.addConfig('user.email','balvinder294@gmail.com');
+// simpleGit.addConfig('user.name','Balvinder Singh');
+// Add remore repo url as origin to repo
+const simpleGitPromise = require('simple-git/promise')();
+simpleGitPromise.addRemote('main',gitHubUrl);
+// Add all files for commit
+  simpleGitPromise.add('.')
+    .then(
+       (addSuccess) => {
+          console.log(addSuccess);
+       }, (failedAdd) => {
+          console.log('adding files failed');
+    });
+// Commit files as Initial Commit
+ simpleGitPromise.commit('Intial commit by simplegit')
+   .then(
+      (successCommit) => {
+        console.log(successCommit);
+     }, (failed) => {
+        console.log('failed commmit');
+ });
+// Finally push to online repository
+ simpleGitPromise.push('origin','master')
+    .then((success) => {
+       console.log('repo successfully pushed');
+    },(failed)=> {
+       console.log('repo push failed');
+ });
